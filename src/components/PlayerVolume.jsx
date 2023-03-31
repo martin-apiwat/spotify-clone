d96455cc6310370c1ad8319b3b0f7437f9d5e300
Martin Apiwat <martin.apiwat@gmail.com>
2023-03-31 10:27:14 +0200

    added stying on volume and player icons

--- src/components/PlayerVolume.jsx
+++ src/components/PlayerVolume.jsx
@@ -8,11 +8,29 @@ export default function PlayerVolume({ player }) {
     return (
         <div className="flex items-center gap-2">
             {volume > 0.5 ? (
-                <Volume2 />
+                <Volume2
+                    className="opacity-80 hover:opacity-100"
+                    onClick={() => {
+                        spotifyApi.setVolume(0);
+                        setVolume(0);
+                    }}
+                />
             ) : volume > 0 ? (
-                <Volume1 />
+                <Volume1
+                    className="opacity-80 hover:opacity-100"
+                    onClick={() => {
+                        spotifyApi.setVolume(0);
+                        setVolume(0);
+                    }}
+                />
             ) : (
-                <VolumeX />
+                <VolumeX
+                    className="opacity-80 hover:opacity-100"
+                    onClick={() => {
+                        spotifyApi.setVolume(40);
+                        setVolume(0.4);
+                    }}
+                />
             )}
 
             <div className="group relative w-36">
