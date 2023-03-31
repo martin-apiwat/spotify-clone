c08183bf91540414690c45fcb02bb2d37fedff8c
Martin Apiwat <martin.apiwat@gmail.com>
2023-03-31 11:55:56 +0200

    added skeleton loader for playlists

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,115 +5,126 @@ import PlayerVolume from "./PlayerVolume";
 import PlayerOverlay from "./PlayerOverlay";
 
 export default function Player() {
-    const [device, setDevice] = useState(null);
-    const [localPlayer, setLocalPlayer] = useState(null);
-    const [track, setTrack] = useState(null);
-    const [isPaused, setIsPaused] = useState(false);
-    const [position, setPosition] = useState(null);
-    const [playerOverlayOpen, setPlayerOverlayOpen] = useState(false);
-    const [isActive, setIsActive] = useState(false);
+   const [device, setDevice] = useState(null);
+   const [localPlayer, setLocalPlayer] = useState(null);
+   const [track, setTrack] = useState(null);
+   const [isPaused, setIsPaused] = useState(false);
+   const [position, setPosition] = useState(null);
+   const [playerOverlayOpen, setPlayerOverlayOpen] = useState(false);
+   const [isActive, setIsActive] = useState(false);
 
-    useEffect(() => {
-        const token = sessionStorage.getItem("spotify-key");
-        const script = document.createElement("script");
-        script.src = "https://sdk.scdn.co/spotify-player.js";
-        script.async = true;
-        document.body.appendChild(script);
+   useEffect(() => {
+      const token = sessionStorage.getItem("spotify-key");
+      const script = document.createElement("script");
+      script.src = "https://sdk.scdn.co/spotify-player.js";
+      script.async = true;
+      document.body.appendChild(script);
 
-        window.onSpotifyWebPlaybackSDKReady = () => {
-            const player = new window.Spotify.Player({
-                name: "Techover Player",
-                getOAuthToken: (cb) => {
-                    cb(token);
-                },
-                volume: 0.5,
-            });
+      window.onSpotifyWebPlaybackSDKReady = () => {
+         const player = new window.Spotify.Player({
+            name: "Techover Player",
+            getOAuthToken: (cb) => {
+               cb(token);
+            },
+            volume: 0.5,
+         });
 
-            console.log("player: ", player);
+         console.log("player: ", player);
 
-            player.addListener("ready", ({ device_id }) => {
-                console.log("Ready with device_id: ", device_id);
-                setDevice(device_id);
-                setLocalPlayer(player);
-            });
-
-            player.addListener("player_state_changed", (state) => {
-                if (!state || !state.track_window?.current_track) {
-                    return;
-                }
-
-                console.log("state changed :", state);
-                setTrack(state.track_window.current_track);
-                setIsPaused(state.paused);
-                setPosition(state.position);
-
-                player.getCurrentState().then((state) => {
-                    if (!state) {
-                        setIsActive(false);
-                    } else {
-                        setIsActive(true);
-                    }
-                });
-            });
+         player.addListener("ready", ({ device_id }) => {
+            console.log("Ready with device_id: ", device_id);
+            setDevice(device_id);
             setLocalPlayer(player);
-            player.connect();
-        };
-    }, []);
+         });
 
-    useEffect(() => {
-        if (!localPlayer) return;
+         player.addListener("player_state_changed", (state) => {
+            if (!state || !state.track_window?.current_track) {
+               return;
+            }
 
-        localPlayer.connect();
+            console.log("state changed :", state);
+            setTrack(state.track_window.current_track);
+            setIsPaused(state.paused);
+            setPosition(state.position);
 
-        return () => {
-            localPlayer.disconnect();
-        };
-    }, [localPlayer]);
+            player.getCurrentState().then((state) => {
+               if (!state) {
+                  setIsActive(false);
+               } else {
+                  setIsActive(true);
+               }
+            });
+         });
+         setLocalPlayer(player);
+         player.connect();
+      };
+   }, []);
 
-    if (!isActive || !localPlayer || !track)
-        return <div>no player, please connect</div>;
+   useEffect(() => {
+      if (!localPlayer) return;
 
-    return (
-        <div>
-            <div
-                className="flex items-center p-4"
-                onClick={() => {
-                    setPlayerOverlayOpen(!playerOverlayOpen);
-                }}
-            >
-                <div className="flex flex-1 items-center">
-                    <img
-                        src={track.album.images[0]?.url}
-                        alt=""
-                        className="mr-4 h-14 w-14 flex-shrink-0"
-                    />
-                    <div className="">
-                        <h4 className="text-sm">{track.name}</h4>
-                        <p className="text-xs text-text-dimmed">
-                            {track.artists[0].name}
-                        </p>
-                    </div>
-                </div>
-                <div className="flex-1 text-center max-md:hidden">
-                    <PlayerControls
-                        player={localPlayer}
-                        isPaused={isPaused}
-                        position={position}
-                        track={track}
-                    />
-                </div>
-                <div className="flex flex-1 justify-end max-md:hidden">
-                    <PlayerVolume player={localPlayer} />
-                </div>
+      localPlayer.connect();
+
+      return () => {
+         localPlayer.disconnect();
+      };
+   }, [localPlayer]);
+
+   useEffect(() => {
+      const playback = async () => {
+         if (device) {
+            await spotifyApi.transferMyPlayback([device], true);
+         }
+         await spotifyApi.getMyDevices();
+      };
+
+      playback();
+   }, [device, spotifyApi]);
+
+   if (!isActive || !localPlayer || !track)
+      return <div>no player, please connect</div>;
+
+   return (
+      <div>
+         <div
+            className="flex items-center p-4"
+            onClick={() => {
+               setPlayerOverlayOpen(!playerOverlayOpen);
+            }}
+         >
+            <div className="flex flex-1 items-center">
+               <img
+                  src={track.album.images[0]?.url}
+                  alt=""
+                  className="mr-4 h-14 w-14 flex-shrink-0"
+               />
+               <div className="">
+                  <h4 className="text-sm">{track.name}</h4>
+                  <p className="text-xs text-text-dimmed">
+                     {track.artists[0].name}
+                  </p>
+               </div>
             </div>
-            <PlayerOverlay
-                setPlayerOverlayOpen={setPlayerOverlayOpen}
-                playerOverlayOpen={playerOverlayOpen}
-                track={track}
-                player={localPlayer}
-                isPaused={isPaused}
-                position={position}
-            />
-        </div>
-    );
+            <div className="flex-1 text-center max-md:hidden">
+               <PlayerControls
+                  player={localPlayer}
+                  isPaused={isPaused}
+                  position={position}
+                  track={track}
+               />
+            </div>
+            <div className="flex flex-1 justify-end max-md:hidden">
+               <PlayerVolume player={localPlayer} />
+            </div>
+         </div>
+         <PlayerOverlay
+            setPlayerOverlayOpen={setPlayerOverlayOpen}
+            playerOverlayOpen={playerOverlayOpen}
+            track={track}
+            player={localPlayer}
+            isPaused={isPaused}
+            position={position}
+         />
+      </div>
+   );
 }
--- src/pages/playlists/[id].jsx
+++ src/pages/playlists/[id].jsx
@@ -5,108 +5,135 @@ import { spotifyApi } from "../_app";
 import { formatTime } from "@/utils/formatTime";
 import { Clock, PlayCircle } from "react-feather";
 import { accessUrl } from "@/config";
+import Link from "next/link";
 
 export default function Playlist() {
-    const router = useRouter();
-    const {
-        data: playlist,
-        isLoading,
-        isError,
-    } = useQuery({
-        queryKey: ["playlists", router.query.id],
-        queryFn: async () =>
-            (await spotifyApi.getPlaylist(router.query.id)).body,
-    });
+   const router = useRouter();
+   const {
+      data: playlist,
+      isLoading,
+      isError,
+   } = useQuery({
+      queryKey: ["playlists", router.query.id],
+      queryFn: async () => (await spotifyApi.getPlaylist(router.query.id)).body,
+   });
 
-    if (isLoading) return <Layout>loading...</Layout>;
-    if (isError)
-        return (
-            <Layout>
-                <div className="">
-                    <h3>Please log in again</h3>
-                    <a
-                        href={accessUrl}
-                        className="rounded-xl bg-primary py-1.5 px-5 text-xl"
-                    >
-                        Sign in
-                    </a>
-                </div>
-            </Layout>
-        );
-
-    return (
-        <Layout>
-            <div className="flex items-end gap-3 bg-gradient-to-b from-primary/70 to-bg-dimmed p-10">
-                <img
-                    src={playlist.images[0]?.url}
-                    alt="playlist image"
-                    className="h-28 w-28 flex-shrink-0 md:h-60 md:w-60"
-                />
-                <div>
-                    <p className="text-sm tracking-widest text-text">
-                        PLAYLIST
-                    </p>
-                    <h2 className="text-3xl font-black md:text-4xl">
-                        {playlist.name}
-                    </h2>
-                </div>
+   if (isError)
+      return (
+         <Layout>
+            <h2 className="px-4 py-16 text-center text-3xl font-bold md:text-5xl">
+               Oops, could not get that playlist
+            </h2>
+            <div className="flex justify-center">
+               <Link href="/" className="rounded-full bg-primary py-4 px-6">
+                  Go back
+               </Link>
             </div>
-            <div className="p-4 md:p-10">
-                <div className="w-full text-text-dimmed">
-                    <div className="grid grid-cols-[auto_1fr_auto] items-center gap-4 px-6 text-sm md:grid-cols-[auto_1fr_1fr_auto]">
-                        <div className="w-8">#</div>
-                        <div>TITLE</div>
-                        <div className="max-md:hidden">ALBUM</div>
-                        <div>
-                            <Clock className="h-5 w-5" />
-                        </div>
-                    </div>
+         </Layout>
+      );
 
-                    <hr className="my-3 border-text-dimmed/40" />
-                    {playlist.tracks.items.map((item, index) => (
-                        <div
-                            key={item.id}
-                            className="group grid grid-cols-[auto_1fr_auto] items-center gap-4 rounded-md py-1.5 px-6 text-sm hover:bg-text-dimmed/10 md:grid-cols-[auto_1fr_1fr_auto]"
-                            onClick={async () => {
-                                await spotifyApi.play({
-                                    context_uri: `spotify:playlist:${router.query.id}`,
-                                    offset: {
-                                        position: index,
-                                    },
-                                });
-                            }}
-                        >
-                            <div className="w-8 text-base">
-                                <p className="group-hover:hidden">
-                                    {index + 1}
-                                </p>
-                                <PlayCircle className="h-5.5 w-5.5 hidden text-text group-hover:block" />
-                            </div>
+   return (
+      <Layout>
+         <div className="flex items-end gap-3 bg-gradient-to-b from-primary/70 to-bg-dimmed p-10">
+            {isLoading ? (
+               <div className="h-28 w-28 flex-shrink-0 animate-pulse  bg-neutral-600 md:h-60 md:w-60"></div>
+            ) : (
+               <img
+                  src={playlist.images[0]?.url}
+                  alt="playlist image"
+                  className="h-28 w-28 flex-shrink-0 md:h-60 md:w-60"
+               />
+            )}
 
-                            <div className="flex items-center gap-4 overflow-hidden">
-                                <img
-                                    src={item.track.album.images[0].url}
-                                    alt=""
-                                    className="h-12 w-12"
-                                />
-                                <div className="overflow-hidden">
-                                    <h4 className=" truncate text-text">
-                                        {item.track.name}
-                                    </h4>
-                                    <p className="truncate">
-                                        {item.track.artists[0].name}
-                                    </p>
+            <div>
+               <p className="text-sm tracking-widest text-text">PLAYLIST</p>
+               {isLoading ? (
+                  <div className="animate-pulse bg-neutral-600 text-3xl font-black text-transparent md:text-4xl">
+                     this is a playlist
+                  </div>
+               ) : (
+                  <h2 className="text-3xl font-black md:text-4xl">
+                     {playlist.name}
+                  </h2>
+               )}
+            </div>
+         </div>
+         <div className="p-4 md:p-10">
+            <div className="w-full text-text-dimmed">
+               <div className="grid grid-cols-[auto_1fr_auto] items-center gap-4 px-6 text-sm md:grid-cols-[auto_1fr_1fr_auto]">
+                  <div className="w-8">#</div>
+                  <div>TITLE</div>
+                  <div className="max-md:hidden">ALBUM</div>
+                  <div>
+                     <Clock className="h-5 w-5" />
+                  </div>
+               </div>
+
+               <hr className="my-3 border-text-dimmed/40" />
+               {isLoading
+                  ? Array(20)
+                       .fill(null)
+                       .map((_, index) => (
+                          <div
+                             key={index}
+                             className=" grid animate-pulse grid-cols-[auto_1fr_auto] items-center gap-4  py-1.5 px-6 text-sm md:grid-cols-[auto_1fr_1fr_auto]"
+                          >
+                             <div className="w-8">
+                                <div className="h-4 w-4 bg-neutral-600"></div>
+                             </div>
+                             <div className="flex gap-4">
+                                <div className="h-12 w-12 flex-shrink-0 bg-neutral-600"></div>
+                                <div className="w-full">
+                                   <div className="mb-2 h-6 w-4/6 rounded-full bg-neutral-600"></div>
+                                   <div className="h-4 w-2/6 rounded-full bg-neutral-600"></div>
                                 </div>
-                            </div>
+                             </div>
+                             <div className="h-6 w-2/6 rounded-full bg-neutral-600 max-md:hidden"></div>
+                             <div className="h-6 w-10 rounded-lg bg-neutral-600"></div>
+                          </div>
+                       ))
+                  : playlist.tracks.items.map((item, index) => (
+                       <div
+                          key={item.id}
+                          className="group grid grid-cols-[auto_1fr_auto] items-center gap-4 rounded-md py-1.5 px-6 text-sm hover:bg-text-dimmed/10 md:grid-cols-[auto_1fr_1fr_auto]"
+                          onClick={async () => {
+                             await spotifyApi.play({
+                                context_uri: `spotify:playlist:${router.query.id}`,
+                                offset: {
+                                   position: index,
+                                },
+                             });
+                          }}
+                       >
+                          <div className="w-8 text-base">
+                             <p className="group-hover:hidden">{index + 1}</p>
+                             <PlayCircle className="h-5.5 w-5.5 hidden text-text group-hover:block" />
+                          </div>
 
-                            <div className="truncate max-md:hidden">
-                                {item.track.album.name}
-                            </div>
-                            <div>{formatTime(item.track.duration_ms)}</div>
-                        </div>
+                          <div className="flex items-center gap-4 overflow-hidden">
+                             <img
+                                src={item.track.album.images[0].url}
+                                alt=""
+                                className="h-12 w-12"
+                             />
+                             <div className="overflow-hidden">
+                                <h4 className=" truncate text-text">
+                                   {item.track.name}
+                                </h4>
+                                <p className="truncate">
+                                   {item.track.artists[0].name}
+                                </p>
+                             </div>
+                          </div>
+
+                          <div className="truncate max-md:hidden">
+                             {item.track.album.name}
+                          </div>
+                          <div>{formatTime(item.track.duration_ms)}</div>
+                       </div>
                     ))}
-                </div>
             </div>
-        </Layout>
-    );
+         </div>
+      </Layout>
+   );
 }
