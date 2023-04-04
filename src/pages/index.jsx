bd6dfd1fe29b41cbfe7f3bc5f22e587cb6d9641c
Martin Apiwat <martin.apiwat@gmail.com>
2023-04-04 20:03:29 +0200

    Fixed message when error

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,45 +4,43 @@ import Link from "next/link";
 import { spotifyApi } from "./_app";
 
 export default function index() {
-    const {
-        data: playlists,
-        isLoading,
-        isError,
-    } = useQuery({
-        queryKey: ["playlists"],
-        queryFn: async () => (await spotifyApi.getUserPlaylists()).body.items,
-    });
+   const {
+      data: playlists,
+      isLoading,
+      isError,
+   } = useQuery({
+      queryKey: ["playlists"],
+      queryFn: async () => (await spotifyApi.getUserPlaylists()).body.items,
+   });
 
-    if (isLoading) return <p>Loading...</p>;
+   if (isLoading) return <p>Loading...</p>;
 
-    if (isError) return <p>Loading...</p>;
+   if (isError) return <p>Error...</p>;
 
-    return (
-        <Layout>
-            <div className="p-10">
-                <h1 className="mb-12 text-4xl font-bold">
-                    Welcome to my Spotify clone!
-                </h1>
+   return (
+      <Layout>
+         <div className="p-10">
+            <h1 className="mb-12 text-4xl font-bold">
+               Welcome to my Spotify clone!
+            </h1>
 
-                <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-                    {playlists.map((playlist) => (
-                        <Link
-                            href={`/playlists/${playlist.id}`}
-                            key={playlist.id}
-                            className="rounded-md bg-bg p-4"
-                        >
-                            <img
-                                src={playlist.images[0]?.url}
-                                alt=""
-                                className="aspect-square"
-                            />
-                            <p className="mt-3 truncate text-sm">
-                                {playlist.name}
-                            </p>
-                        </Link>
-                    ))}
-                </div>
+            <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+               {playlists.map((playlist) => (
+                  <Link
+                     href={`/playlists/${playlist.id}`}
+                     key={playlist.id}
+                     className="rounded-md bg-bg p-4"
+                  >
+                     <img
+                        src={playlist.images[0]?.url}
+                        alt=""
+                        className="aspect-square"
+                     />
+                     <p className="mt-3 truncate text-sm">{playlist.name}</p>
+                  </Link>
+               ))}
             </div>
-        </Layout>
-    );
+         </div>
+      </Layout>
+   );
 }
